be693351602069efc4792a125d052d94cb463f39
nitin0016 <nitinanttal258@gmail.com>
2023-03-16 11:59:46 +0530

    updated

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$8_$)dsr#j72z8@(kg_$cst)5&lf9klecb+s*a_7lmma&^3shy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,8 +123,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT="staticfiles"
 MEDIA_ROOT=BASE_DIR/'media'
 MEDIA_URL='/media/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR, 'C:/Users/Parveen/Desktop/blogproject/blog/blogapp/static'),)
-
+STATICFILES_DIRS=[
+    BASE_DIR/'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
